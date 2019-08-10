c0576081d9105653faf4e41d4ef70fad6544fa8f
briantam23 <briantam23@gmail.com>
2019-08-10 14:27:26 -0400

    Added useState & useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './store';
+import axios from 'axios';
 import './App.css';
 
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const fetchUsers = async () => {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+    setUsers(res.data);
+  };
+  useEffect(() => { fetchUsers(users) });
+
   const dispatch = useDispatch();
   const count = useSelector(store => store.count);
 
   return (
     <div>
       <h1>The count is { count }</h1>
-      <button onClick={() => dispatch(increment(count))}>+</button>
-      <button onClick={() => dispatch(decrement(count))}>-</button>
+      <button onClick={ () => dispatch(increment(count)) }>+</button>
+      <button onClick={ () => dispatch(decrement(count)) }>-</button>
+      <h1>Fetched Users</h1>
+      <ul>
+      { users.map(user => <li key={ user.id }>{ user.name }</li>) }
+      </ul>
     </div>
   );
 }
